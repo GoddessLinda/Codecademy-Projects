91976917070e8ba9cd468586d33f8b024f8a15fa
Linda <30485253+GoddessLinda@users.noreply.github.com>
2021-07-01 18:18:55 -0700

    Add files via upload
    tweaking js script

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-*/  Mixed Messages */ 
-*/ Silly Cow Jokes */
+/*  Mixed Messages */ 
+/* Silly Cow Jokes */
+/* cow joke #1 Bells and Horns */
 console.log("Why do cows have bells?"); 
 console.log("Because their horns don't work!"); 
+/* cow joke #2 No Milk */ 
+console.log("What do you call a cow that stps giving milk?")
+console.log("An udder failure!"); 
+/* cow joke #3 Beef */ 
+console.log("What do you call a cow with no legs?");
+console.log("Ground beef!"); 
+/* closing message */ 
+console.log("Holy Cow, these are corny."); 
