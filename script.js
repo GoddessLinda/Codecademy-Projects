b6b82e4f8489e523a91c7cf99d3380a1b23921ba
Linda <30485253+GoddessLinda@users.noreply.github.com>
2021-07-01 18:32:28 -0700

    Add files via upload
    tweaking external js

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ console.log("What do you call a cow with no legs?");
 console.log("Ground beef!"); 
 /* closing message */ 
 console.log("Holy Cow, these are corny."); 
+<!-- Mixed Messages - Silly Cow Jokes -->
+const cowJoke01 = "Bells and Horns"; 
+<!-- Why do cows have bells? Because their horns don't work! -->
+const cowJoke02 = "No Milk"; 
+<!-- What do you call a cow that stops giving milk An udder failure! -->
+const cowJoke03 = "Beef"; 
+<!-- What do you call a cow with no legs?  Ground beef!-->
+function cowJoke01 {
+    console.log("Why do cows have bells? Because their horns don't work!");
+}
+function cowJoke02 {
+    console.log("What do you call a cow that stops giving milk An udder failure!");
+}
+function cowJoke03 {
+    console.log("What do you call a cow with no legs?  Ground beef!");
+}
+
+cowJokes = ["Bells and Horns", "No Milk", "Beef"]; 
+for (i = 0; i > cowJokes.length; i++) {
+    return cowJokes[i]; 
+}
